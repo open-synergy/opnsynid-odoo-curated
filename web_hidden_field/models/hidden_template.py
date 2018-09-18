d42119963a61a1e4eac72a3056234a34697dec9a
Andhitia Rama <andhitia.r@gmail.com>
2018-09-18 19:58:22 +0700

    [BACKPORT][8.0] web_hidden_field

--- web_hidden_field/models/hidden_template.py
+++ web_hidden_field/models/hidden_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from openerp import models, fields
 
 
 class HiddenTemplateField(models.Model):
